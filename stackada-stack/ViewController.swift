dc7064b35c1e44a44cae7d95df10fdfd1d8d90dc
matthew-zak <matthew.zak@tooploox.com>
2017-04-27 16:45:11 -0400

    Stack views

---
+++ stackada-stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackada-stack
+//
+//  Created by Admin on 25/04/2017.
+//  Copyright © 2017 Mattowy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
